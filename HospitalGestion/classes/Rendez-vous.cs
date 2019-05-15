fc88c1b369143c623d9fa9de15ed47b0acd4caba
Remi <yanbuaban.r@gmail.com>
2019-05-15 14:29:29 +0200

    Ajout du menu

--- HospitalGestion/classes/Rendez-vous.cs
+++ HospitalGestion/classes/Rendez-vous.cs
@@ -12,12 +12,12 @@ namespace HospitalGestion.classes
         private int codeRDV;
         private string medecin;
         private DateTime date_RDV;
-        private ServiceEnum service;
+        //private ServiceEnum service;
 
         public int CodeRDV { get => codeRDV; set => codeRDV = value; }
         public string Medecin { get => medecin; set => medecin = value; }
         public DateTime Date_RDV { get => date_RDV; set => date_RDV = value; }
-        public ServiceEnum Service { get => service; set => service = value; }
+        //public ServiceEnum Service { get => service; set => service = value; }
     }
 
 
