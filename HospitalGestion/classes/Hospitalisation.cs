24477eb1d97b84a2cbbf1bcd6963eb855ebf7c03
vlaboure <V.laboure@gmail.com>
2019-05-15 11:33:05 +0200

    Classes patient et hopitalisation

---
+++ HospitalGestion/classes/Hospitalisation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalGestion.classes
+{
+    class Hospitalisation
+    {
+        private string idHopitalisation;
+        private DateTime dateAdmission;
+        private string typeAdmission;
+        private string motifAdmission;
+        private string medecinTraitant;
+        private string nomAccompagnant;
+        private string preNomAccompagnant;
+        private string lienParente;
+        private DateTime dateEntreeAcc;
+        private DateTime dateSortieAcc;
+        private DateTime dateSortie;
+        private string motifSortie;
+        private string resultatSortie;
+        private DateTime dateDeces;
+        private string motifDeces;
+
+        public string IdHopitalisation { get => idHopitalisation; set => idHopitalisation = value; }
+        public DateTime DateAdmission { get => dateAdmission; set => dateAdmission = value; }
+        public string TypeAdmission { get => typeAdmission; set => typeAdmission = value; }
+        public string MotifAdmission { get => motifAdmission; set => motifAdmission = value; }
+        public string MedecinTraitant { get => medecinTraitant; set => medecinTraitant = value; }
+        public string NomAccompagnant { get => nomAccompagnant; set => nomAccompagnant = value; }
+        public string PreNomAccompagnant { get => preNomAccompagnant; set => preNomAccompagnant = value; }
+        public string LienParente { get => lienParente; set => lienParente = value; }
+        public DateTime DateEntreeAcc { get => dateEntreeAcc; set => dateEntreeAcc = value; }
+        public DateTime DateSortieAcc { get => dateSortieAcc; set => dateSortieAcc = value; }
+        public DateTime DateSortie { get => dateSortie; set => dateSortie = value; }
+        public string MotifSortie { get => motifSortie; set => motifSortie = value; }
+        public string ResultatSortie { get => resultatSortie; set => resultatSortie = value; }
+        public DateTime DateDeces { get => dateDeces; set => dateDeces = value; }
+        public string MotifDeces { get => motifDeces; set => motifDeces = value; }
+    }
+
+}
---
+++ HospitalGestion/classes/Patient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalGestion.classes
+{
+    class Patient
+    {
+        private string idPatient;
+        private string nom;
+        private string prenom;
+        private DateTime dateNaissance;
+        private SexeEnum sex;
+        private int adresse;
+        private string situation;
+        private string assuranceMedicale;
+        private string codeAssurance;
+        private string tel;
+        private string nomPere;
+        private string nomMère;
+        private string telPersAprevenir;
+
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom1 { get => prenom; set => prenom = value; }
+        public DateTime DateNaissance { get => dateNaissance; set => dateNaissance = value; }
+        public int Adresse { get => adresse; set => adresse = value; }
+        public string Situation { get => situation; set => situation = value; }
+        public string AssuranceMedicale { get => assuranceMedicale; set => assuranceMedicale = value; }
+        public string CodeAssurance { get => codeAssurance; set => codeAssurance = value; }
+        public string Tel { get => tel; set => tel = value; }
+        public string NomPere { get => nomPere; set => nomPere = value; }
+        public string NomMère { get => nomMère; set => nomMère = value; }
+        public string TelPersAprevenir { get => telPersAprevenir; set => telPersAprevenir = value; }
+        public string IdPatient { get => idPatient; set => idPatient = value; }
+        public SexeEnum Sex { get => sex; set => sex = value; }
+    }
+
+    public enum SexeEnum
+    {
+        homme,
+        femme
+    }
+}
