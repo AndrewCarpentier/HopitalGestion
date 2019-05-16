b5198f4af3772b805ad22db17ebb9081eb5fc8f4
vlaboure <V.laboure@gmail.com>
2019-05-16 09:54:35 +0200

    creation interface dbcommand

--- HospitalGestion/bdd/DBCommand.cs
+++ HospitalGestion/bdd/DBCommand.cs
@@ -1,4 +1,5 @@
 ﻿using HospitalGestion.classes;
+using HospitalGestion.interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,15 @@ using System.Threading.Tasks;
 
 namespace HospitalGestion.bdd
 {
-    public class DBCommand
+    public class DBCommand : IDB
     {
         public void CrerChambres(int n)
         {
-          
+            SqlCommand = new SqlCommand()
+            for (int i = 1; i <= n; i++)
+            {
+
+            }
         }
 
     }
