4921647648febcb6f7a6042a6e7312ba5f4bfa68
AndrewCarpentier <andr3wcarpentier@gmail.com>
2019-06-07 14:23:46 +0200

    correction viewmodel

---
+++ HopitalGestionLinqToSql/Services/ConnectionService.cs
@@ -0,0 +1,42 @@
+﻿using HospitalGestionWPF.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopitalGestionWPF.Service
+{
+    public class ConnectionService
+    {
+        public bool valider(string  password)
+        {
+            bool b = true;
+            ConnexionPatientWindow mP;
+            switch (password)
+            {
+                case "administration":
+                    MenuAdministrationWindow mA = new MenuAdministrationWindow();
+                    mA.Show();
+                    break;
+                case "generaliste":
+                    mP = new ConnexionPatientWindow();
+                    mP.Show();
+                    break;
+                case "secretaire":
+                    mP = new ConnexionPatientWindow();
+                    mP.Show();
+                    break;
+                case "specialiste":
+                    mP = new ConnexionPatientWindow();
+                    mP.Show();
+                    break;
+                default:
+                    b = false;
+                    break;
+            }
+
+            return b;
+        }
+    }
+}
